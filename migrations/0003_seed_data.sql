35c0e326c83618c198e09f2fb3e448335e8f6658
parthodastwo <52148667+parthodastwo@users.noreply.github.com>
2025-07-10 09:43:28 +0000

    AWS Deployment: More Bug Fixes

--- migrations/0003_seed_data.sql
+++ migrations/0003_seed_data.sql
@@ -1,5 +1,5 @@
-insert into users (username, password, first_name, last_name, role, is_active) 
-values ('admin', 'admin', 'Partha', 'Das','admin', true));
+insert into users (username, password, email, first_name, last_name, role, is_active) 
+values ('admin', '$2b$10$JsOR7YmlRIQ0ZxeW3EvEXu4UUc.aF5jxuF7HMMEEdQbYzZ4lc2lHy', 'pdas@health-roads.com''Partha', 'Das','admin', true);
 
 Insert into srcm_canonical (name, description, data_type, is_active) 
 values ('Client Unique Identifier','Client Unique Identifier', 'string', true);
@@ -14,10 +14,7 @@ Insert into srcm_canonical (name, description, data_type, is_active)
 values ('Client Last Name	','Client Last Name	', 'string', true);
 
 Insert into srcm_canonical (name, description, data_type, is_active) 
-values ('Client Date of Birth,'Client Date of Birth', 'date', true);
-
-Insert into srcm_canonical (name, description, data_type, is_active) 
-values ('Client First Name','Client First Name', 'string', true);
+values ('Client Date of Birth','Client Date of Birth', 'date', true);
 
 Insert into srcm_canonical (name, description, data_type, is_active) 
 values ('Client Gender','Client Gender', 'string', true);
